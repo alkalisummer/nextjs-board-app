30a909936688c5b24a764b8a22336951ab6391ab
khkang <alkalisummer@gmail.com>
2023-06-16 12:56:23 +0900

    post 신규 등록시 작성한 게시글로 router설정, css 수정

--- app/posts/create/page.tsx
+++ app/posts/create/page.tsx
@@ -9,34 +9,55 @@ const CreatePost = () => {
   const router = useRouter();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    await fetch('http://127.0.0.1:8090/api/collections/posts/records', {
+    const request = await fetch('http://127.0.0.1:8090/api/collections/posts/records', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         title,
         content,
       }),
-    });
-    setTitle('');
-    setContent('');
-    router.refresh();
+    })
+      .then((response) => response.json())
+      .then(function (res) {
+        debugger;
+        setTitle('');
+        setContent('');
+        router.refresh();
+        router.push(`/posts/detail/${res.id}`);
+      });
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type='text'
-        className='post_title_input'
-        placeholder='제목을 입력하세요'
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
+    <form
+      className='post_div'
+      onSubmit={handleSubmit}>
+      <div className='post_title_created'>
+        <input
+          type='text'
+          className='post_title_input'
+          placeholder='제목을 입력하세요'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
       <textarea
         className='post_content_textarea'
         placeholder='내용을 입력하세요'
         value={content}
         onChange={(e) => setContent(e.target.value)}
       />
-      <button type='submit'>완료</button>
+      <div className='post_btn_div'>
+        <button
+          className='post_cancel_btn'
+          onClick={() => router.push(`/`)}
+          type='button'>
+          취소
+        </button>
+        <button
+          className='post_submit_btn'
+          type='submit'>
+          완료
+        </button>
+      </div>
     </form>
   );
 };
