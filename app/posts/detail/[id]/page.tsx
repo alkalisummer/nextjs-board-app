5e7364f3c407e168d2365d450c863cfde5861a2e
khkang <alkalisummer@gmail.com>
2023-06-27 17:21:49 +0900

    게시글 삭제시 오라클 클라우드 스토리지 저장된 해당 게시글이미지 파일도 같이 삭제되게 수정

--- app/posts/detail/[id]/page.tsx
+++ app/posts/detail/[id]/page.tsx
@@ -9,7 +9,11 @@ import { timeFormat } from '@/app/utils/commonUtils';
 
 const PostDetailPage = ({ params }: any) => {
   const [post, setPost] = useState({ post_title: '', post_cntn: '', post_html_cntn: { data: [] }, amnt_dttm: '' });
+  const [imgFileArr, setImgFileArr] = useState<string[]>([]);
   const router = useRouter();
+
+  //html data 추출
+  const cheerio = require('cheerio');
 
   const param = {
     type: '',
@@ -20,6 +24,16 @@ const PostDetailPage = ({ params }: any) => {
     param.type = 'read';
     await axios.get('/api/handlePost', { params: param }).then((res) => {
       setPost(res.data.items[0]);
+
+      //html 데이터 추출
+      const htmlCntn = Buffer.from(res.data.items[0].post_html_cntn).toString();
+      const $ = cheerio.load(htmlCntn);
+
+      //기존 이미지 파일 이름 추출
+      const imageTags = $('img');
+      const currImageArr = imageTags.map((index: number, el: any) => $(el).attr('alt')).get();
+
+      setImgFileArr(currImageArr);
     });
   };
 
@@ -31,6 +45,11 @@ const PostDetailPage = ({ params }: any) => {
     param.type = 'delete';
     await axios.get('/api/handlePost', { params: param }).then(() => {
       setPost({ post_title: '', post_cntn: '', post_html_cntn: { data: [] }, amnt_dttm: '' });
+
+      // 해당 게시글 이미지 삭제
+      let removedImg = imgFileArr;
+      axios.post('/api/deleteImgFile', { removedImg });
+
       router.refresh();
       router.push('/');
     });
